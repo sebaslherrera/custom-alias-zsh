88ea53e86b60118e6c161adb195456276e452450
Sebastian Lopez H <lopez.sebastian@correounivalle.edu.co>
2019-08-14 09:43:06 -0500

    Create alias.zsh
    added for apache2 and postgresql services

---
+++ alias.zsh
@@ -0,0 +1,4 @@
+#Put the alias here echo $ZSH_CUSTOM
+
+alias stop_ap="sudo service apache2 stop & sudo service postgresql stop"
+alias start_ap="sudo service apache2 start & sudo service postgresql start"
