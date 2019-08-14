821224076d48b461348efbb0365f1b46d460295d
Sebastian Lopez H <lopez.sebastian@correounivalle.edu.co>
2019-08-14 09:50:09 -0500

    Alias for the folder where the .zsh runs
    Alias for one project added

--- alias.zsh
+++ alias.zsh
@@ -1,4 +1,7 @@
 #Put the alias here echo $ZSH_CUSTOM
 
+alias alias_folder="cd $ZSH_CUSTOM"
+
 alias stop_ap="sudo service apache2 stop & sudo service postgresql stop"
 alias start_ap="sudo service apache2 start & sudo service postgresql start"
+alias superdrogas="cd /home/dintev/Documentos/sebaspersonal/django/SaaS_SuperDrogas"
