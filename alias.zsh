59abd88c0d25ee895ead943e23a6e1ee601b7a3b
Sebastian Lopez H <lopez.sebastian@correounivalle.edu.co>
2019-08-15 10:28:33 -0500

    update

--- alias.zsh
+++ alias.zsh
@@ -2,6 +2,16 @@
 
 alias alias_folder="cd $ZSH_CUSTOM"
 
+# General 
 alias stop_ap="sudo service apache2 stop & sudo service postgresql stop"
 alias start_ap="sudo service apache2 start & sudo service postgresql start"
+
+
+# Work
 alias superdrogas="cd /home/dintev/Documentos/sebaspersonal/django/SaaS_SuperDrogas"
+alias moove="cd /var/www/html/moodle35/theme/moove"
+
+# Pc
+
+
+# Laptop
