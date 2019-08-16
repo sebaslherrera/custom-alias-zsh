b385817d843e64b53ca3a257cbb0fb80fcc91cfd
sebas119Laptop <lopez.sebastian@correounivalle.edu.co>
2019-08-15 19:31:19 -0500

    zsh-auto install alias for that and .zshr

--- alias.zsh
+++ alias.zsh
@@ -1,11 +1,13 @@
-#Put the alias here echo $ZSH_CUSTOM
+# Put the alias here echo $ZSH_CUSTOM
 
 alias alias_folder="cd $ZSH_CUSTOM"
+
+# Put in plugins -->  plugins=(git zsh-autosuggestions django)
+alias zsh_config="nano ~/.zshrc"
 
 # General 
 alias stop_ap="sudo service apache2 stop & sudo service postgresql stop"
 alias start_ap="sudo service apache2 start & sudo service postgresql start"
-
 
 # Work
 alias superdrogas="cd /home/dintev/Documentos/sebaspersonal/django/SaaS_SuperDrogas"
@@ -29,3 +31,10 @@ alias delvolume="docker volume prune"
 
 # Recreate the volumes
 alias rebuild="docker-compose up --build"
+
+
+# Oh My Zsh plugins
+
+alias install_plugins="git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+"
+
