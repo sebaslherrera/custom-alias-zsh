44fb95eb6378cf8d470537a6323760d8b00faaee
Sebas Vagrant Lenovo <lopez.sebastian@correounivalle.edu.co>
2019-10-31 15:17:25 +0000

    Alias updated in holberton

--- alias.zsh
+++ alias.zsh
@@ -1,37 +1,12 @@
-# Put the alias here echo $ZSH_CUSTOM
-
 alias alias_folder="cd $ZSH_CUSTOM"
 # My favourite themes is ZSH_THEME="gnzh"
 # Put in plugins -->  plugins=(git django docker git-flow pipenv pip python zsh-autosuggestions)
 alias zsh_config="nano ~/.zshrc"
 
-# General 
+# General
 alias stop_ap="sudo service apache2 stop & sudo service postgresql stop"
 alias start_ap="sudo service apache2 start & sudo service postgresql start"
 
-# Work
-alias superdrogas="cd /home/dintev/Documentos/sebaspersonal/django/SaaS_SuperDrogas"
-alias moove="cd /var/www/html/moodle35/theme/moove"
-
-# Pc sebas119
-
-alias superdrogas_pc="cd /home/sebas119/Documents/django/project/SaaS_SuperDrogas"
-alias monster_pc="cd /home/sebas119/Documents/django/project/monstertemplate/monster-admin/HTML/main"
-
-# Laptop anton2
-
-alias superdrogas_laptop="cd /home/anton2/Documents/django/SaaS_SuperDrogas"
-
-# Docker
-
-# Delete all the containers
-alias delcontainers="docker rm $(docker ps -a -q) -f"
-
-# Delete all the modules
-alias delvolume="docker volume prune"
-
-# Recreate the volumes
-alias rebuild="docker-compose up --build"
 
 # Oh My Zsh plugins
 
@@ -40,6 +15,4 @@ alias install_plugins="git clone https://github.com/zsh-users/zsh-autosuggestion
 
 # Projects Alias
 
-# Install superdrogas project
-alias makesuper="docker exec -it saas_pg psql -U postgres -d multitenant -f /home/_datos_iniciales/1_tenant_publico.sql && docker-compose restart"
 
